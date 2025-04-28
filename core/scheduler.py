20ccf14ea975c9d6175434dd00690de92e6a1961
Zachary 'Foxxin' Bukolt <foxxinthetechguy@proton.me>
2025-04-28 19:02:00 -0400

    Improve output from script.

--- core/scheduler.py
+++ core/scheduler.py
@@ -12,14 +12,16 @@ def load_tasks():
     for i in tasks['tasks']:
         print(i)
 
+print("Scheduler: Starting...")
 #Check if tasks.json file exists and if it does not then create it.
 jsonExists = os.path.exists(tasks_json_path)
 
-if jsonExists == False:    
+if jsonExists == False: 
+    print("Scheduler: tasks.json does not exist.")   
     with open(tasks_json_path, 'w', encoding='utf-8') as tasks_json:
         tasks_json.write('{\n  "tasks": []\n}')
-        print("tasks.json was created.")
+        print("Scheduler: tasks.json created.")
 else:
-    print("tasks.json already exists.")
+    print("Scheduler: tasks.json already exists.")
 
 load_tasks()
