fbbe2297348ae8982be60e378a1939d3aaa980eb
Zachary 'Foxxin' Bukolt <foxxinthetechguy@proton.me>
2025-04-27 23:59:39 -0400

    Update task_json var to task_json_path for less confusion.

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,22 +1,22 @@
 import os
 import json
 
-tasks_json = "../data/tasks.json"
+tasks_json_path = "../data/tasks.json"
 
 #Opens tasks.json file, parses the json, and prints the list.
 def load_tasks():
-    tasks_json = "../data/tasks.json"
-    with open(tasks_json, 'r') as tasks_json:
+    tasks_json_path = "../data/tasks.json"
+    with open(tasks_json_path, 'r') as tasks_json:
         tasks = json.load(tasks_json)
+    
     for i in tasks['tasks']:
         print(i)
-    tasks_json.close()
 
 #Check if tasks.json file exists and if it does not then create it.
-jsonExists = os.path.exists(tasks_json)
+jsonExists = os.path.exists(tasks_json_path)
 
 if jsonExists == False:    
-    with open(tasks_json, 'w', encoding='utf-8') as tasks_json:
+    with open(tasks_json_path, 'w', encoding='utf-8') as tasks_json:
         tasks_json.write("[]")
         print("tasks.json was created.")
 else:
