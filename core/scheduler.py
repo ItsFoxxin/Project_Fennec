6121dcbc906911046b81de412afe1bfceb8248a5
Zachary 'Foxxin' Bukolt <foxxinthetechguy@proton.me>
2025-04-27 23:23:44 -0400

    Add load_task().

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,12 +1,25 @@
 import os
+import json
+
+tasks_json = "../data/tasks.json"
+
+#Opens tasks.json file, parses the json, and prints the list.
+def load_tasks():
+    tasks_json = "../data/tasks.json"
+    with open(tasks_json, 'r') as tasks_json:
+        tasks = json.load(tasks_json)
+    for i in tasks['tasks']:
+        print(i)
+    tasks_json.close()
 
 #Check if tasks.json file exists and if it does not then create it.
-jsonExists = os.path.exists("../data/tasks.json")
+jsonExists = os.path.exists(tasks_json)
 
 if jsonExists == False:    
-    file_path = "../data/tasks.json"
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write("[]")
+    with open(tasks_json, 'w', encoding='utf-8') as tasks_json:
+        tasks_json.write("[]")
         print("tasks.json was created.")
 else:
     print("tasks.json already exists.")
+
+load_tasks()
